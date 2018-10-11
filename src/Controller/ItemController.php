bcbf70d9f2ebc37f82bde24416de2fd7d1b6189a
Eric Rousselet <rousselet.eric@gmail.com>
2018-10-11 21:44:16 +0200

    ajout upload preview + messages erreurs formulaire

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -88,76 +88,75 @@ class ItemController extends AbstractController
      */
     public function add()
     {
+        $errors=array();
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             if($_POST){
-                $errors=array();
 
                 if (empty($_POST['highlight'])) {
                     $_POST['highlight']="off";
                 }
 
                 if(empty($_POST['name'])) {
-                    $errors['name'] = '<h3>Veuillez remplir le champ "Nom"</h3>';
+                    $errors['name'] = 'Veuillez remplir le champ "Nom';
                 }
                 if(empty($_POST['category'])) {
-                    $errors['category'] = '<h3>Veuillez remplir le champ "Catégorie"</h3>';
+                    $errors['category'] = 'Veuillez remplir le champ "Catégorie"';
                 }
                 if(empty($_POST['price'])) {
-                    $errors['price'] = '<h3>Veuillez remplir le champ "Prix"</h3>';
+                    $errors['price'] = 'Veuillez remplir le champ "Prix"';
                 }
                 if(empty($_POST['description'])) {
-                    $errors['description'] = '<h3>Veuillez remplir le champ "Description"</h3>';
+                    $errors['description'] = 'Veuillez remplir le champ "Description"';
                 }
 
                 if (!preg_match("/^[a-zA-Z0-9 ]+$/", $_POST['name'])){
-                    $errors['name'] = '<h3>Veuillez remplir le champ "Nom" uniquement avec des caractères alphanumériques</h3>';
+                    $errors['name'] = 'Veuillez remplir le champ "Nom" uniquement avec des caractères alphanumériques';
                 }
                 if (!preg_match("/^[a-zA-Z0-9 \.\(\)]+$/", $_POST['description'])){
-                    $errors['description'] = '<h3>Veuillez remplir le champ "Description" uniquement avec des caractères alphanumériques et les caractères suivants : " ", ".", "(", ")"</h3>';
+                    $errors['description'] = 'Veuillez remplir le champ "Description" uniquement avec des caractères alphanumériques et les caractères suivants : " ", ".", "(", ")"';
                 }
                 if ( (!preg_match("/^[a-zA-Z0-9 \.\(\)]+$/", $_POST['review'])) &&  (!empty($_POST['review']))) {
-                    $errors['review'] = '<h3>Veuillez remplir le champ "Avis de la boutique" uniquement avec des caractères alphanumériques et les caractères suivants : " ", ".", "(", ")"</h3>';
+                    $errors['review'] = 'Veuillez remplir le champ "Avis de la boutique" uniquement avec des caractères alphanumériques et les caractères suivants : " ", ".", "(", ")"';
                 }
 
                 if (!preg_match("/^[a-z]+$/", $_POST['category'])){
-                    $errors['category'] = '<h3>Veuillez remplir le champ "Catégorie" uniquement avec des caractères alphabétiques</h3>';
+                    $errors['category'] = 'Veuillez remplir le champ "Catégorie" uniquement avec des caractères alphabétiques';
                 }
                 if (!preg_match("/^[a-z]+$/", $_POST['highlight'])){
-                    $errors['highlight'] = '<h3>Veuillez remplir le champ "Mise en avant" uniquement avec des caractères alphabétiques</h3>';
+                    $errors['highlight'] = 'Veuillez remplir le champ "Mise en avant" uniquement avec des caractères alphabétiques';
                 }
 
                 if (!preg_match("/^[0-9]+$/", $_POST['price'])){
-                    $errors['price'] = '<h3>Veuillez remplir le champ "Prix" uniquement avec des caractères numériques</h3>';
+                    $errors['price'] = 'Veuillez remplir le champ "Prix" uniquement avec des caractères numériques';
                 }
 
                 if (strlen($_POST['name'])>255){
-                    $errors['name'] = '<h3>Veuillez remplir le champ "Nom" avec 255 caractères maximum</h3>';
+                    $errors['name'] = 'Veuillez remplir le champ "Nom" avec 255 caractères maximum';
                 }
                 if (strlen($_POST['category'])>255){
-                    $errors['category'] = '<h3>Veuillez remplir le champ "Catégorie" avec 255 caractères maximum</h3>';
+                    $errors['category'] = 'Veuillez remplir le champ "Catégorie" avec 255 caractères maximum';
                 }
                 if (strlen(strval($_POST['price']))>11){
-                    $errors['price'] = '<h3>Veuillez remplir le champ "Prix" avec 11 caractères maximum</h3>';
+                    $errors['price'] = 'Veuillez remplir le champ "Prix" avec 11 caractères maximum';
                 }
                 if (strlen($_POST['highlight'])>3){
-                    $errors['highlight'] = '<h3>Veuillez remplir le champ "Mise en avant" avec 3 caractères maximum</h3>';
+                    $errors['highlight'] = 'Veuillez remplir le champ "Mise en avant" avec 3 caractères maximum';
                 }
                 if (strlen($_POST['description'])>5000){
-                    $errors['description'] = '<h3>Veuillez remplir le champ "Description" avec 5000 caractères maximum</h3>';
+                    $errors['description'] = 'Veuillez remplir le champ "Description" avec 5000 caractères maximum';
                 }
                 if ((strlen($_POST['review'])>5000) && (!empty($_POST['review']))){
-                    $errors['review'] = '<h3>Veuillez remplir le champ "Avis de la boutique" avec 5000 caractères maximum</h3>';
+                    $errors['review'] = 'Veuillez remplir le champ "Avis de la boutique" avec 5000 caractères maximum';
                 }
 
                 if (!empty($_FILES['picture']['name'])) {
                     $extension = pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
                     if (($extension != "png") && ($extension != "jpg")) {
-                        $errors['picture'] = '<h3>Veuillez télécharger une image au format jpg ou png uniquement</h3>';
+                        $errors['picture'] = 'Veuillez télécharger une image au format jpg ou png uniquement';
                     }
                 }
-                var_dump($errors);
 
                 if(0==count($errors)) {
                     $itemManager = new ItemManager($this->getPdo());
@@ -192,7 +191,8 @@ class ItemController extends AbstractController
             }
         }
 
-        return $this->twig->render('Item/add.html.twig');
+        return $this->twig->render('Item/add.html.twig', ['errors' => $errors, 'placeholders' => $_POST
+        ]);
     }
 
 
