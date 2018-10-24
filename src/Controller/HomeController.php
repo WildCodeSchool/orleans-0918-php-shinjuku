e25439c8505e6e823adb21894f2c9263c43bd3f9
NicoFrancois <42697477+NicoFrancois@users.noreply.github.com>
2018-10-24 11:32:16 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,29 +1,18 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 16:07
- * PHP version 7
+ * User: thomas
+ * Date: 12/10/18
+ * Time: 09:36
  */
-
 namespace Controller;
-
 use Model\Home;
 use Model\HomeManager;
 
-
 class HomeController extends AbstractController
 {
-
-
-
-    public function article()
+    public function index()
     {
-        $articlesManager = new HomeManager($this->getPdo());
-        $articles = $articlesManager->selectHighlight();
-        return $this->twig->render('Home/home.html.twig', ['article' => $articles]);
+        return $this->twig->render('/Home/home.html.twig');
     }
-
-
 }
